82651b5ce1513e0db8815ada7fa19af01b2b3ca4
Werefox <jack@wrightus.com>
2022-04-11 18:26:19 -0400

    Outlined game functionality in script.js and added the colors to the css file

---
+++ script.js
@@ -0,0 +1,6 @@
+// how the game works:
+// the player chooses a grid size and difficulty
+// the game builds a layout
+// the game generates clues based on the difficulty
+// the player solves the puzzle using the clues and places the tokens in the correct positions
+// the game checks that all of the tokens are correct
