a26a9494b93cb80199af526f190969255fdcc70b
Werefox <jack@wrightus.com>
2022-04-11 19:41:19 -0400

    Added puzzle generation

--- script.js
+++ script.js
@@ -6,7 +6,7 @@
 // the game checks that all of the tokens are correct
 
 function main() {
-
+	const size = 3
 	// build the game grid
 	function buildGrid(size) {
 		// get the parent grid
@@ -33,9 +33,65 @@ function main() {
 		gameBox.style.gridTemplateColumns = gridLines
 		gameBox.style.gridTemplateRows = gridLines
 	}
-	buildGrid(2)
+	buildGrid(size)
 
-	function generatePuzzle(size) {
+	// function for building a list of tokens
+	function getTokenList(size) {
+		// list alphabet to reference in the token names
+		const alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+		let tokens = [ ]
+		for (let x = 0; x < size; x++) {
+
+			for (let y = 0; y < size; y++) {
+
+				let token = alphabet[x] + (y + 1)
+				tokens.push(token)
+			}
+		}
+		return tokens
 	}
+	console.log(getTokenList(size))
+
+	// function to build the puzzle layout
+	function buildPuzzle(size) {
+		// get tokens
+		let tokens = getTokenList(size)
+
+		let puzzle = [ ]
+		let i = 0
+		for (let x = 0; x < size; x++) {
+			let row = [ ]
+
+			for (let y = 0; y < size; y++) {
+				row.push(tokens[i])
+				i++
+			}
+
+			puzzle.push(row)
+		}
+
+		return puzzle
+	}
+
+	console.log(buildPuzzle(size))
 }
+
+// shuffle function sourced from https://bost.ocks.org/mike/shuffle/
+function shuffle(array) {
+	var m = array.length, t, i;
+  
+	// While there remain elements to shuffle…
+	while (m) {
+  
+	  // Pick a remaining element…
+	  i = Math.floor(Math.random() * m--);
+  
+	  // And swap it with the current element.
+	  t = array[m];
+	  array[m] = array[i];
+	  array[i] = t;
+	}
+  
+	return array;
+  }
