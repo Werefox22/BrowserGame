c65897e4e4df423b5165f008b1fc3fea545b08fc
Werefox <jack@wrightus.com>
2022-04-11 19:14:22 -0400

    Added a grid generator

--- script.js
+++ script.js
@@ -5,7 +5,29 @@
 // the player solves the puzzle using the clues and places the tokens in the correct positions
 // the game checks that all of the tokens are correct
 
-// build the game grid
-function buildGrid(size) {
+function main() {
 
+	// build the game grid
+	function buildGrid(size) {
+		const gameBox = document.querySelector("#box")
+
+		let gridLines = ""
+
+		for (let x = 0; x < size; x++) {
+			gridLines += "auto "
+
+			for (let y = 0; y < size; y++) {
+
+				let grid = document.createElement("div")
+
+				grid.classList.add("grid")
+
+				gameBox.append(grid)
+			}
+		}
+
+		gameBox.style.gridTemplateColumns = gridLines
+		gameBox.style.gridTemplateRows = gridLines
+	}
+	buildGrid(2)
 }
